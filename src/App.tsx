b6495c4062a0395a0ae5d5cf235e4ce1f4b3fa84
Mohammad Baqer <mtbaqer@gmail.com>
2020-08-13 13:59:31 -0400

    added coming soon card

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,38 @@
 import React from "react";
-import { ReactComponent as CaseIcon } from "./assets/icons/CaseIcon.svg";
+import { ReactComponent as Logo } from "./assets/icons/Logo.svg";
 import "./App.css";
 import styled from "styled-components";
 
 function App() {
   return (
     <Container>
-      <Title>وظيفة</Title>
-      <CaseIcon width="20px" style={{ transform: "scale(1.5)" }} />
+      <Logo style={{ transform: "scale(1.25)", margin: "20px" }} />
+      <Card>
+        <CardContent>...قادمون قاريباً</CardContent>
+      </Card>
     </Container>
   );
 }
 
 let Container = styled.div`
-  background: linear-gradient(180deg, #6c46ab 0%, #3d206c 100%);
+  background: #efebf5;
   height: 100vh;
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
 `;
 
-let Title = styled.p`
-  color: white;
-  margin-right: 12px;
-  font-family: "Noto Kufi Arabic";
-  font-size: 30px;
+let Card = styled.div`
+  background: #ffffff;
+  box-shadow: 0px 34px 74px rgba(39, 52, 107, 0.12);
+  border-radius: 12px;
+  padding: 20px 80px;
+`;
+
+let CardContent = styled.p`
+  font-size: 20px;
+  color: #37333e;
 `;
 
 export default App;
