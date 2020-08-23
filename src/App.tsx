9af15a95270b8301a205b8ccde307db340b02660
Mohammad Baqer <mtbaqer@gmail.com>
2020-08-23 10:15:28 -0400

    added a basic main page with Title

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,9 @@
 import React from "react";
-import { ReactComponent as Logo } from "./assets/icons/Logo.svg";
 import "./App.css";
-import styled from "styled-components";
+import MainPage from "./pages/MainPage";
 
 function App() {
-  return (
-    <Container>
-      <StyledLogo />
-      <Card>
-        <CardContent>
-          <Flickering>...</Flickering> قادمون قريباً
-        </CardContent>
-      </Card>
-    </Container>
-  );
+  return <MainPage />;
 }
-
-let StyledLogo = styled(Logo)`
-  transform: scale(1.25);
-  margin: 20px;
-`;
-
-let Container = styled.div`
-  background: #efebf5;
-  height: 100vh;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-`;
-
-let Card = styled.div`
-  background: #ffffff;
-  box-shadow: 0px 34px 74px rgba(39, 52, 107, 0.12);
-  border-radius: 12px;
-  padding: 20px 80px;
-`;
-
-let CardContent = styled.p`
-  font-size: 20px;
-  color: #37333e;
-`;
-
-let Flickering = styled.span`
-  @keyframes flickering {
-    from {
-      opacity: 0;
-    }
-    to {
-      opacity: 1;
-    }
-  }
-  animation: flickering 0.7s alternate infinite;
-`;
 
 export default App;
---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,36 @@
+import React, { FunctionComponent } from "react";
+import styled from "styled-components";
+
+const MainPage: FunctionComponent = (props) => {
+  return (
+    <PageContainer>
+      <Container>
+        <BigText>المكان المناسب للعثور على وظيفة في الكويت</BigText>
+      </Container>
+    </PageContainer>
+  );
+};
+
+const PageContainer = styled.div`
+  height: 100vh;
+  display: flex;
+`;
+
+const Container = styled.div`
+  height: 40%;
+  flex-grow: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  background: linear-gradient(180deg, #6c46ab 0%, #3d206c 100%);
+`;
+
+const BigText = styled.p`
+  color: #ffffff;
+  font-weight: bold;
+  font-size: 40px;
+  width: 742px;
+`;
+
+export default MainPage;
