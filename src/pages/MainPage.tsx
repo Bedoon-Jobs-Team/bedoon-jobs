eb75e941a045da78e82b590d50aff5a75e97b1a0
Mohammad Baqer <mtbaqer@gmail.com>
2020-08-27 13:28:38 -0400

    added cards container and page count

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -2,13 +2,18 @@ import React, { FunctionComponent } from "react";
 import styled from "styled-components";
 import Header from "../components/Header";
 
-const MainPage: FunctionComponent = props => {
+const MainPage: FunctionComponent = (props) => {
   return (
     <PageContainer>
       <Header />
       <Container>
         <BigText>المكان المناسب للعثور على وظيفة في الكويت</BigText>
       </Container>
+      <ContentContainer>
+        <CardsContainer>
+          <PageCount>صفحة 1 من 120 صفحة</PageCount>
+        </CardsContainer>
+      </ContentContainer>
     </PageContainer>
   );
 };
@@ -16,11 +21,11 @@ const MainPage: FunctionComponent = props => {
 const PageContainer = styled.div`
   height: 100vh;
   display: flex;
+  flex-direction: column;
 `;
 
 const Container = styled.div`
   height: 426px;
-  flex-grow: 1;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -33,6 +38,25 @@ const BigText = styled.p`
   font-weight: bold;
   font-size: 40px;
   width: 742px;
+`;
+
+const ContentContainer = styled.div`
+  background-color: #f7f5fa;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 100px 200px 80px 200px;
+`;
+
+const CardsContainer = styled.div`
+  width: 1042px;
+  flex-shrink: 1;
+`;
+
+const PageCount = styled.p`
+  font-size: 14px;
+  color: #9891a3;
+  margin-bottom: 16px;
 `;
 
 export default MainPage;
