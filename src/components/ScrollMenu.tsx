ac35d6af04d5f29fd72a96660001633e59716332
Sara Alkhawari <sara.a.alkhawari@gmail.com>
2020-08-29 13:11:16 +0300

    added scroll menu bottuns and arrow

--- src/components/ScrollMenu.tsx
+++ src/components/ScrollMenu.tsx
@@ -1,9 +1,27 @@
 import React, { FunctionComponent } from "react";
-import { ReactComponent as LogoSmall } from "../assets/icons/LogoSmall.svg";
+import { ReactComponent as Arrow } from "../assets/icons/arrow.svg";
 import styled from "styled-components";
 
 const ScrollMenu: FunctionComponent = props => {
-  return <Menu></Menu>;
+  return (
+    <Menu>
+      <Container>
+                <ActiveButton>جميع الوظائف</ActiveButton>
+                <InactiveButton>هندسة</InactiveButton>
+                <InactiveButton>طب وتمريض</InactiveButton>
+                <InactiveButton>مناديب</InactiveButton>
+                <InactiveButton>أمن وسلامة</InactiveButton>
+                <InactiveButton>سكرتارية</InactiveButton>
+                <InactiveButton>رياضة</InactiveButton>
+                <InactiveButton>وظائف إدارية</InactiveButton>
+      </Container>
+      <ScrollArow>
+                
+        <StyledArrow />
+              
+      </ScrollArow>
+    </Menu>
+  );
 };
 
 const Menu = styled.div`
@@ -15,7 +33,41 @@ const Menu = styled.div`
   background-color: #ffffff;
   box-shadow: 0px 34px 74px rgba (39,  52,  107,  0.12);
   border-radius: 15px;
-  padding: 0px 10px;
+`;
+
+const Container = styled.div`
+  display: flex;
+  overflow-x: scroll;
+`;
+
+const ActiveButton = styled.div`
+  background-color: #f8507b;
+  color: #ffffff;
+  border-radius: 12px;
+  border: 1px solid #ffffff;
+  float: right;
+  padding: 10px 28px;
+  margin-top: 10px;
+`;
+
+const InactiveButton = styled(ActiveButton)`
+  background-color: #ebe3f6;
+  color: #332d3c;
+`;
+
+const ScrollArow = styled.div`
+  position: absolute;
+  width: 29px;
+  height: 77px;
+  left: 0px;
+  top: 0px;
+  background-color: #ffffff;
+  box-shadow: 20px 4px 40px rgba (22,  8,  46,  0.1);
+  border-radius: 12px 0px 0px 12px;
+`;
+
+const StyledArrow = styled(Arrow)`
+  margin-top: 0px;
 `;
 
 export default ScrollMenu;
