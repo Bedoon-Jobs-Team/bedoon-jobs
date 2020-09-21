2febb0fead75cb35964524cb332a1a57f81cff43
Mohammad Baqer <mtbaqer@gmail.com>
2020-09-21 06:37:59 +0300

    made the switching scroll to the top

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,18 @@ import "./App.css";
 import JobDetails from "./pages/JobDetails";
 import MainPage from "./pages/MainPage";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import ScrollToTop from "./utils/ScrollToTop";
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route exact path="/" component={MainPage} />
-        <Route exact path="/listings/:id" component={JobDetails} />
-      </Switch>
+      <>
+        <ScrollToTop />
+        <Switch>
+          <Route exact path="/" component={MainPage} />
+          <Route exact path="/listings/:id" component={JobDetails} />
+        </Switch>
+      </>
     </Router>
   );
 }
