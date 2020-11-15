4d825c95c07302c778ff1f7358deb4e8147a2f6a
Mohammad Baqer <mtbaqer@gmail.com>
2020-11-15 21:36:43 +0300

    setted up firebase

---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+import firebase from "firebase/app";
+
+const config = {
+  apiKey: "AIzaSyAUCIh6-DXs6m8J5Yos-j8EWZSeJxH1vkM",
+  authDomain: "project-bidoon.firebaseapp.com",
+  databaseURL: "https://project-bidoon.firebaseio.com",
+  projectId: "project-bidoon",
+  storageBucket: "project-bidoon.appspot.com",
+  messagingSenderId: "771917087128",
+  appId: "1:771917087128:web:61494ed20796ae7095abed",
+  measurementId: "G-NPP98XZ40L",
+};
+
+firebase.initializeApp(config);
+firebase.analytics();
+
+export default firebase;
